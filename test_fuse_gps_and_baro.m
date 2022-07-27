aff1293b13c21e0c67e9e7ebb81c69ea88c223c3
pichimeter <michernstpet@gmail.com>
2022-07-27 22:32:40 +0200

    Minor changes

--- test_fuse_gps_and_baro.m
+++ test_fuse_gps_and_baro.m
@@ -1,7 +1,7 @@
 clc, clear all
 %%
 
-is_matlab = false;  % false -> octave
+is_matlab = true;  % false -> octave
 
 if is_matlab
   addpath ../fcn_bib
@@ -14,12 +14,12 @@ Ts = 1/40;
 downsample_divider = 4;
 
 % create a similar function like ctzsnooze
-N = 25*downsample_divider;
+N = 50*downsample_divider;
 time = (0:N-1).' *Ts;
-alt = -cos(2*pi*0.3*time) + 0.3;
+alt = -cos(2*pi*0.3*5*time) + 0.3;
 alt = cumtrapz(time, alt);
 alt = alt - alt(1);
-alt = [zeros(10,1); alt(1:N-10)]*10;
+alt = [zeros(10,1); alt(1:N-10)]*3;
 dalt_end = mean(diff(alt(N-10:N))) / Ts;
 alt = [alt; alt(end) + Ts*dalt_end + time(1:N/4) * dalt_end];
 N = N + N/4;
@@ -39,20 +39,21 @@ for i = 1:N
     downsample_cntr = downsample_cntr + 1;
 end
 
-% add  noise and/or offset
-apply_corruption = 1;
-baro_offset = apply_corruption * ones(N,1);
-baro_noise = apply_corruption * cumtrapz(time, sqrt(0.1) * randn(N,1));
+% add noise and/or offset
+apply_offset = 1;
+apply_noise = 0;
+baro_offset = apply_offset * ones(N,1);
+baro_noise = apply_noise * cumtrapz(time, sqrt(0.1) * randn(N,1));
 baro_alt = baro_alt + baro_offset + baro_noise;
-gps_noise = apply_corruption * cumtrapz(time, sqrt(0.1) * randn(N,1));
+gps_noise = apply_noise * cumtrapz(time, sqrt(0.1) * randn(N,1));
 gps_alt = gps_alt + gps_noise;
 
 % fuse the data accoring to the trust values, this will be the dc
-gps_trust = 0.7;
+gps_trust = 0.5;
 alt_dc_fused = gps_trust * gps_alt + (1 - gps_trust) * baro_alt;
 
 % fuse the dc with a complementary filter and baro
-f_cut = 0.3; % Hz
+f_cut = 1.5; % Hz
 Gpt3 = tf(get_filter('pt3', f_cut, Ts));
 alt_fused = filter(Gpt3.num{1}, Gpt3.den{1}, alt_dc_fused) + baro_alt - filter(Gpt3.num{1}, Gpt3.den{1}, baro_alt);
 
@@ -71,6 +72,17 @@ xlabel('Time (sec)'), ylabel('Altitude (m)'), xlim([0 time(end)])
 legend('ideal altitude', 'alt dc fused', 'alt fused', 'location', 'northwest')
 
 figure(3)
-stairs(time(1:end-1), diff([alt, alt_dc_fused, alt_fused]), 'LineWidth', lw), grid on
-xlabel('Time (sec)'), ylabel('d/dt Altitude (m)'), xlim([0 time(end)])
+subplot(121)
+stairs(time(1:end-1), diff([alt, alt_dc_fused, alt_fused]/Ts), 'LineWidth', lw), grid on
+xlabel('Time (sec)'), ylabel('d/dt Altitude (m/s)'), xlim([0 time(end)])
 legend('ideal altitude', 'alt dc fused', 'alt fused', 'location', 'northwest')
+subplot(122)
+stairs(time(1:end-2), diff(diff([alt, 0*alt_dc_fused, alt_fused]))/Ts^2, 'LineWidth', lw), grid on
+xlabel('Time (sec)'), ylabel('d2/dt2 Altitude (m2/s2)'), xlim([0 time(end)])
+legend('ideal altitude', 'alt dc fused', 'alt fused', 'location', 'northwest')
+
+figure(4)
+subplot(121)
+bode(Gpt3, 1 - Gpt3, 2*pi*logspace(-2, log10(1/2/Ts), 2e3)), grid on
+subplot(122)
+step(Gpt3, 1- Gpt3, 4), grid on
